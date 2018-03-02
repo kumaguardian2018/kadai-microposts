d350602afae1c038b05649002a4131d098b8c426
kumaguardian <kuma.guardian2018@gmail.com>
2018-03-02 20:52:32 +0000

    user pages

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -1,14 +1,32 @@
-    public function show($id)
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use App\User;
+
+class UsersController extends Controller
+{
+    public function index()
+    {
+        $users = User::all();
+        
+        return view('users.index', [
+            'users' => $users,
+        ]);
+    }
+    
+        public function show($id)
     {
         $user = User::find($id);
-        $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
         
-        $data = [
+        return view('users.show', [
             'user' => $user,
-            'microposts' => $microposts,
-        ];
-        
-        $data += $this->counts($user);
-        
-        return view('users.show', $data);
+        ]);
     }
+}
+
